355bdfed0ed17fdee0a22c5886320643725687b5
zhenglian <zhenglian@192.168.56.1>
2016-08-14 16:52:15 +0800

    修改服务，支持jar打包

--- src/test/java/com/zheng/test/ServerTest.java
+++ src/test/java/com/zheng/test/ServerTest.java
@@ -1,13 +1,10 @@
 package com.zheng.test;
 
-import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ServerTest {
-
-	@Test
-	@SuppressWarnings("resource")
-	public void startServer() throws Exception {
+	
+	public static void main(String[] args) throws Exception {
 		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(new String[] { "applicationContext.xml" });
 		ctx.start();
 		System.out.println("服务启动成功!");
