0ee10654a51e85ec0f992571aeef1d5917e824ca
zhenglian <zhenglian@192.168.56.1>
2016-08-20 18:14:47 +0800

    添加mock测试

--- src/main/java/com/zheng/service/impl/HelloServiceImpl.java
+++ src/main/java/com/zheng/service/impl/HelloServiceImpl.java
@@ -2,11 +2,11 @@ package com.zheng.service.impl;
 
 import org.springframework.stereotype.Service;
 
+import com.alibaba.dubbo.rpc.RpcException;
 import com.zheng.service.HelloService;
 
 @Service("helloService")
 public class HelloServiceImpl implements HelloService {
-	
 	@Override
 	public String sayHello(String name) {
 		return "hello " + name;
